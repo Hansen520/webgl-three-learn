02e3a679ee603d6e81ab5bec8c5cf00a89edb530
金汉生 <jinhansheng@gonglu.info>
2024-06-20 18:15:53 +0800

    update

---
+++ 4.2结合vue框架/three/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+
+  <Parent />
+</template>
+
+<style scoped>
+</style>
---
+++ 4.2结合vue框架/three/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Date: 2024-06-20 11:29:21
+ * @Description: description
+-->
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
